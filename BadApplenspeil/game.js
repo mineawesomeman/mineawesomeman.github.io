bc31b3e0cf16bf0a8b5b010b0db793b7cdd21d21
David Rosenstein <mineawesomeman@gmail.com>
2024-02-03 11:36:20 -0500

    added debug feature to help time this

--- BadApplenspeil/game.js
+++ BadApplenspeil/game.js
@@ -2,7 +2,7 @@
 
 let frames = Array.apply(null, Array(6566)).map(function () {}); //initize empty array of size 6566, which will have all the loaded images
 let loadCount = 0; //this keeps track of how many frames have been loaded
-let currentFrame = -30;
+let currentFrame = -35;
 let timer = null;
 let audioChannel = null;
 let finishedLoading = false;
@@ -77,3 +77,6 @@ function nextFrame() {
 	}
 }
 
+PS.touch = function (_, _, _, _) {
+	PS.debug(`${currentFrame}`);
+}
